2b9918c6da6b123dcff768adcedda89f266d682c
juliusw624-sys <juliusw624@gmail.com>
2026-06-17 21:06:11 +0300

    Add Rectangle.java - Rectangle shape implementation

---
+++ Rectangle.java
@@ -0,0 +1,30 @@
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(String color, boolean filled, double width, double height)
+            throws InvalidShapeException {
+        super(color, filled);
+        if (width <= 0 || height <= 0)
+            throw new InvalidShapeException("Invalid dimensions");
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public void resize(double factor) throws InvalidShapeException {
+        if (factor <= 0) throw new InvalidShapeException("Invalid factor");
+        width *= factor;
+        height *= factor;
+    }
+}
